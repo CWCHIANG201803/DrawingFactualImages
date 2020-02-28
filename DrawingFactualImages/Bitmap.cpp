b8c98bf949359ab02fbea171c432428beb7ba3ca
ChaoWei <harry2300042@gmail.com>
2020-02-28 11:10:09 +1100

    Lec76 completed
    allocate memory for bitmap

--- DrawingFactualImages/Bitmap.cpp
+++ DrawingFactualImages/Bitmap.cpp
@@ -1,11 +1,14 @@
-
-
 #include "Bitmap.hpp"
 
 namespace caveofprogramming {
 
-	Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height) {
+	Bitmap::Bitmap(int width, int height) : 
+		m_width(width), 
+		m_height(height), 
+		m_pPixel(new uint8_t[width*height*3]) {
+
 	}
+
 	bool Bitmap::write(string filename){
 		return false;
 	}
@@ -14,6 +17,7 @@ namespace caveofprogramming {
 	}
 
 	Bitmap::~Bitmap(){  
+
 	} 
 	
 }
--- DrawingFactualImages/Bitmap.hpp
+++ DrawingFactualImages/Bitmap.hpp
@@ -3,6 +3,7 @@
 #define BITMAP_HPP
 #include <string>
 #include <cstdint>
+#include <memory>
 using namespace std;
 
 namespace caveofprogramming {
@@ -10,6 +11,8 @@ namespace caveofprogramming {
 	private:
 		int m_width{ 0 };
 		int m_height{ 0 };
+		unique_ptr<uint8_t[]> m_pPixel{nullptr};
+
 	public:
 		Bitmap(int width, int height);
 		void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
