9880193a57af92dfa1015617aacc339cf011adaf
ChaoWei <harry2300042@gmail.com>
2020-02-28 14:00:20 +1100

    Lec78 completed
    setting bitmap pixels

--- DrawingFactualImages/Bitmap.cpp
+++ DrawingFactualImages/Bitmap.cpp
@@ -1,7 +1,7 @@
 #include "Bitmap.hpp"
 #include "BitmapInfoHeader.hpp"
 #include "BitmapFileHeader.hpp"
-
+#include <fstream>
 using namespace caveofprogramming;
 
 namespace caveofprogramming {
@@ -9,7 +9,7 @@ namespace caveofprogramming {
 	Bitmap::Bitmap(int width, int height) : 
 		m_width(width), 
 		m_height(height), 
-		m_pPixel(new uint8_t[width * height * 3]{}) {
+		m_pPixels(new uint8_t[width * height * 3]{}) {
 
 	}
 
@@ -23,16 +23,37 @@ namespace caveofprogramming {
 
 		fileHeader.dataOffset = sizeof(BitmapFileHeader)
 			+ sizeof(BitmapInfoHeader);
-
-		infoHeader.width = m_width;
 		infoHeader.height = m_height;
+		infoHeader.width = m_width;
 
+		ofstream file;
+		file.open(filename, ios::out | ios::binary);
 
+		if (!file) return false;
 
-		return false;
+		file.write((char*)&fileHeader, sizeof(fileHeader));
+		file.write((char*)&infoHeader, sizeof(infoHeader));
+		file.write((char*)m_pPixels.get(), m_width*m_height*3);
+		
+		file.close();
+		if (!file) return false;
+
+		return true;
 	}
 	void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue)
 	{
+		uint8_t* pPixel = m_pPixels.get();
+
+		pPixel += (y * 3) * m_width + (x * 3);
+
+		// because of little Endian
+		pPixel[0] = blue;
+		pPixel[1] = green;
+		pPixel[2] = red;
+
+
+
+		// 0xFF8833
 	}
 
 	Bitmap::~Bitmap(){  
--- DrawingFactualImages/main.cpp
+++ DrawingFactualImages/main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
-#include "BitmapFileHeader.hpp"  
-#include "BitmapInfoHeader.hpp"
+#include "Bitmap.hpp"
 using namespace std;
-
+using namespace caveofprogramming;
 
 int main(int argc, char* argv[])
 {
-	cout << "hello world" << endl;
+	int const WIDTH = 800;
+	int const HEIGHT = 600;
+
+	Bitmap bitmap(WIDTH, HEIGHT);
+
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			bitmap.setPixel(x, y, 255, 0, 0);
+		}
+	}
+
+	bitmap.setPixel(WIDTH/2, HEIGHT/2,255,255,255);
+	  
+	bitmap.write("test.bmp");
+
+
+	cout << "Finished" << endl;
 	return 0;
 }
